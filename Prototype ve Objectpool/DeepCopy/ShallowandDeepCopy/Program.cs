3bb42a27d56543ec31d20a22b2a3ed9a56c9c68b
Hüseyin Fırat Albayrak <35110958+frat57@users.noreply.github.com>
2020-04-15 03:46:27 +0300

    Add files via upload

---
+++ Prototype ve Objectpool/DeepCopy/ShallowandDeepCopy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ShallowandDeepCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Name = "Firat";
+            emp1.Department = "Yazılım Geliştirme";
+            Employee emp2 = emp1.GetClone();
+            emp2.Name = "Zeki";
+            emp2.Department = "İK";
+
+            Console.WriteLine("Emplpyee 1: ");
+            Console.WriteLine("Name: " + emp1.Name + ", Department: " + emp1.Department);
+            Console.WriteLine("Emplpyee 2: ");
+            Console.WriteLine("Name: " + emp2.Name + ", Department: " + emp2.Department);
+            Console.Read();
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        //shallow copy and deep copy different
+        public Employee GetClone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+
+}
---
+++ Prototype ve Objectpool/Shallow Copy/ShallowAndDeppCopy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ShallowAndDeppCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Name = "Firat";
+            emp1.Department = "Yazılım Geliştirme";
+            Employee emp2 = emp1;
+            emp2.Name = "Zeki";
+            emp2.Department = "İK";
+
+            Console.WriteLine("Emplpyee 1: ");
+            Console.WriteLine("Name: " + emp1.Name + ", Department: " + emp1.Department);
+            Console.WriteLine("Emplpyee 2: ");
+            Console.WriteLine("Name: " + emp2.Name + ", Department: " + emp2.Department);
+            Console.Read();
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        //shallow copy and deep copy different
+        public Employee GetClone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+
+
+}
